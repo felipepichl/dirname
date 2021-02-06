738bdf843a88df2dac42f5b56961377b0d8d93f7
Felipe <felipe.pichl@hotmail.com>
2021-02-06 10:30:55 -0300

    Working in Appointment

---
+++ projects/gonutrition/server/src/app/services/CreateAppointmentService.ts
@@ -0,0 +1,37 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import Appointment from '@models/Appointment';
+import AppointmentRepository from '../repositories/AppointmentRepository';
+
+interface Request {
+  date: Date;
+  provider: string;
+}
+
+class CreateAppointmentService {
+  public async execute({ date, provider }: Request): Promise<Appointment> {
+    const appointmentRepository = getCustomRepository(AppointmentRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new Error('This appointment is already booked');
+    }
+
+    const appointment = appointmentRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    await appointmentRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
