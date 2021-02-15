633f129d7608a44370c5c805b872299344d0db85
Felipe <felipe.pichl@hotmail.com>
2021-02-15 17:21:51 -0300

    GoNutrition - Adding Controllers

--- projects/gonutrition/server/src/modules/users/infra/http/routes/users.routes.ts
+++ projects/gonutrition/server/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,49 +1,24 @@
 import { Router } from 'express';
-import { container } from 'tsyringe';
+
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
-import CreateUserService from '@modules/users/services/CreateUserServices';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import UsersController from '../controllers/UsersController';
 
 import ensureAuthenticate from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const usersController = new UsersController();
 
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', async (request, response) => {
-  const { name, email, password } = request.body;
-
-  const createUser = container.resolve(CreateUserService);
-
-  const user = await createUser.execute({
-    name,
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return response.json(user);
-});
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
   '/avatar',
   ensureAuthenticate,
   upload.single('avatar'),
-  async (request, response) => {
-    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-
-    const user = await updateUserAvatar.execute({
-      user_id: request.user.id,
-      avatarFileName: request.file.filename,
-    });
-
-    delete user.password;
-
-    return response.json(user);
-  },
+  usersController.update,
 );
 
 export default usersRouter;
