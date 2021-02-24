27ed15f0eb0dcea8ba1616abef3cbd4850d1d3ea
Felipe Pichl <felipe.pichl@hotmail.com>
2021-02-23 21:17:05 -0300

    GoNutrition - Starting TDD

---
+++ projects/gonutrition/server/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,32 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+
+describe('SendForgotPassworEmail', () => {
+  it('should be able to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEmailService = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'jonhdoe@example.com',
+      password: 'hash123',
+    });
+
+    sendForgotPasswordEmailService.execute({
+      email: 'jonhdoe@example.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+});
