ce08ab305c177ab21f3931d2f0c6853a7486c392
Felipe <felipe.pichl@hotmail.com>
2021-02-24 16:38:54 -0300

    GoNutrition - Password recovery test

--- projects/gonutrition/server/src/shared/container/index.ts
+++ projects/gonutrition/server/src/shared/container/index.ts
@@ -6,6 +6,9 @@ import './providers';
 import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
+// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+// import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
