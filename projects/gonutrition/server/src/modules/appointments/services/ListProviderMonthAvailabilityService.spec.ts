38e47e26974afdbb6fb250bb853783dffbd52b8c
Felipe Pichl <felipe.pichl@hotmail.com>
2021-03-04 16:02:12 -0300

    GoNutrition - End list provider month Service

--- projects/gonutrition/server/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ projects/gonutrition/server/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -11,17 +11,54 @@ describe('ListProviderMonthAvailability', () => {
   beforeEach(() => {
     fakeAppointmentsRepository = new FakeAppointmentsRepository();
 
-    listProviderMonthAvalability = new ListProviderMonthAvailabilityService();
+    listProviderMonthAvalability = new ListProviderMonthAvailabilityService(
+      fakeAppointmentsRepository,
+    );
   });
 
   it('should be able to list the month availability from provider', async () => {
     await fakeAppointmentsRepository.create({
-      date: new Date(2021, 2, 13, 8, 0, 0),
+      date: new Date(2021, 2, 13, 9, 0, 0),
       provider_id: 'provider',
     });
 
     await fakeAppointmentsRepository.create({
-      date: new Date(2021, 2, 13, 9, 0, 0),
+      date: new Date(2021, 2, 13, 10, 0, 0),
+      provider_id: 'provider',
+    });
+
+    await fakeAppointmentsRepository.create({
+      date: new Date(2021, 2, 13, 11, 0, 0),
+      provider_id: 'provider',
+    });
+
+    await fakeAppointmentsRepository.create({
+      date: new Date(2021, 2, 13, 12, 0, 0),
+      provider_id: 'provider',
+    });
+
+    await fakeAppointmentsRepository.create({
+      date: new Date(2021, 2, 13, 13, 0, 0),
+      provider_id: 'provider',
+    });
+
+    await fakeAppointmentsRepository.create({
+      date: new Date(2021, 2, 13, 14, 0, 0),
+      provider_id: 'provider',
+    });
+
+    await fakeAppointmentsRepository.create({
+      date: new Date(2021, 2, 13, 15, 0, 0),
+      provider_id: 'provider',
+    });
+
+    await fakeAppointmentsRepository.create({
+      date: new Date(2021, 2, 13, 16, 0, 0),
+      provider_id: 'provider',
+    });
+
+    await fakeAppointmentsRepository.create({
+      date: new Date(2021, 2, 13, 17, 0, 0),
       provider_id: 'provider',
     });
 
@@ -31,7 +68,7 @@ describe('ListProviderMonthAvailability', () => {
     });
 
     const availability = await listProviderMonthAvalability.execute({
-      user_id: 'provider',
+      provider_id: 'provider',
       month: 3,
       year: 2021,
     });
@@ -40,7 +77,7 @@ describe('ListProviderMonthAvailability', () => {
       expect.arrayContaining([
         { day: 12, available: true },
         { day: 13, available: false },
-        { day: 14, available: false },
+        { day: 14, available: true },
         { day: 15, available: true },
       ]),
     );
--- projects/gonutrition/server/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ projects/gonutrition/server/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,14 +1,15 @@
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 // import AppError from '@shared/errors/AppError';
 
 // import User from '@modules/users/infra/typeorm/entities/User';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
 
 interface IRequest {
-  /** user_id is provider */
-  user_id: string;
-  month: number;
+  provider_id: string;
   year: number;
+  month: number;
 }
 
 type IResponse = Array<{
@@ -18,10 +19,43 @@ type IResponse = Array<{
 
 @injectable()
 class ListProviderMonthAvailabilityService {
-  constructor() {}
+  constructor(
+    @inject('AppointmentRepository')
+    private appointmentRepository: IAppointmentsRepository,
+  ) {}
 
-  public async execute({ user_id, month, year }: IRequest): Promise<IResponse> {
-    return [{ day: 1, available: false }];
+  public async execute({
+    provider_id,
+    month,
+    year,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentRepository.findAllInMonthFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+      },
+    );
+
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 9,
+      };
+    });
+
+    return availability;
   }
 }
 
