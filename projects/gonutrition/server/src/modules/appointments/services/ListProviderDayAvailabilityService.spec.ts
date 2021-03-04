f743699ff48a7a4359a166652f0cd75180277942
Felipe Pichl <felipe.pichl@hotmail.com>
2021-03-04 19:59:21 -0300

    GoNutrition - End list provider day Service

--- projects/gonutrition/server/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ projects/gonutrition/server/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -18,28 +18,35 @@ describe('ListProviderDayAvailability', () => {
 
   it('should be able to list the day availability from provider', async () => {
     await fakeAppointmentsRepository.create({
-      date: new Date(2021, 2, 13, 9, 0, 0),
+      date: new Date(2021, 2, 13, 14, 0, 0),
       provider_id: 'provider',
     });
 
     await fakeAppointmentsRepository.create({
-      date: new Date(2021, 2, 13, 11, 0, 0),
+      date: new Date(2021, 2, 13, 15, 0, 0),
       provider_id: 'provider',
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2021, 2, 13, 12).getTime();
     });
 
     const availability = await listProviderDayAvalability.execute({
       provider_id: 'provider',
-      month: 3,
       year: 2021,
+      month: 3,
       day: 13,
     });
 
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 9, available: false },
-        { hour: 10, available: true },
+        { hour: 10, available: false },
         { hour: 11, available: false },
-        { hour: 12, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
