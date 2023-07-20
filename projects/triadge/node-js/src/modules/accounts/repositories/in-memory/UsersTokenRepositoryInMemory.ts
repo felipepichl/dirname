c38775417fade3c7c8fa8a9590a9fd50af1b829d
Felipe Pichl <felipe.pichl@hotmail.com>
2023-07-20 14:06:02 -0300

    Triadge: UserTokensRepository in memory

--- projects/triadge/node-js/src/modules/accounts/repositories/in-memory/UsersTokenRepositoryInMemory.ts
+++ projects/triadge/node-js/src/modules/accounts/repositories/in-memory/UsersTokenRepositoryInMemory.ts
@@ -1,38 +1,31 @@
-import { ICreateUserTokensDTO } from '@modules/accounts/dtos/ICreateUserTokensDTO';
-import { UserTokens } from '@modules/accounts/infra/prisma/models/UserTokens';
+import { UserTokens } from '@modules/accounts/domain/UserTokens';
 
 import { IUsersTokensRepository } from '../IUsersTokensRepository';
 
 class UsersTokenRepositoryInMemory implements IUsersTokensRepository {
-  private userTokens: UserTokens[] = [];
+  private usersTokens: UserTokens[] = [];
 
-  async create({
-    user_id,
-    refresh_token,
-    expires_date,
-  }: ICreateUserTokensDTO): Promise<UserTokens> {
-    const userTokens = new UserTokens();
+  async create(userTokens: UserTokens): Promise<UserTokens> {
+    this.usersTokens.push(userTokens);
 
-    Object.assign(userTokens, { user_id, refresh_token, expires_date });
-
-    this.userTokens.push(userTokens);
-
-    return userTokens;
+    return this.usersTokens[0];
   }
   async findByUserIdAndRefreshToken(
     user_id: string,
     refresh_token: string,
   ): Promise<UserTokens> {
-    return this.userTokens.find(
-      userToken =>
-        userToken.fk_user_id === user_id &&
-        userToken.refresh_token === refresh_token,
+    return this.usersTokens.find(
+      userTokens =>
+        userTokens.user_id === user_id &&
+        userTokens.refresh_token === refresh_token,
     );
   }
   async deleteById(id: string): Promise<void> {
-    const index = this.userTokens.findIndex(userToken => userToken.id === id);
+    const index = this.usersTokens.findIndex(
+      userTokens => userTokens.id.toString() === id,
+    );
 
-    this.userTokens.slice(index);
+    this.usersTokens.splice(index);
   }
 }
 
