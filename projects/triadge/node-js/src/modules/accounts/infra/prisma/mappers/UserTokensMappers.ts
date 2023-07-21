26a4ee6c5c37302719d9ba1bd4b37a094f3a9385
Felipe Pichl <felipe.pichl@hotmail.com>
2023-07-21 11:23:16 -0300

    Triadge: UserTokensMappers

---
+++ projects/triadge/node-js/src/modules/accounts/infra/prisma/mappers/UserTokensMappers.ts
@@ -0,0 +1,26 @@
+import { UserTokens } from '@modules/accounts/domain/UserTokens';
+import { UserTokens as RawUserTokens } from '@prisma/client';
+
+import { IMapper } from '@shared/core/infra/Mapper';
+
+class UserTokensMappers implements IMapper<UserTokens, RawUserTokens> {
+  toPersistence(object: UserTokens): UserTokens {
+    return object;
+  }
+  toDomain({
+    fk_user_id,
+    expires_date,
+    refresh_token,
+  }: RawUserTokens): UserTokens {
+    return UserTokens.createUserTokens({
+      user_id: fk_user_id,
+      expires_date,
+      refresh_token,
+    });
+  }
+  getMapper(): IMapper<UserTokens, RawUserTokens> {
+    return new UserTokensMappers();
+  }
+}
+
+export { UserTokensMappers };
