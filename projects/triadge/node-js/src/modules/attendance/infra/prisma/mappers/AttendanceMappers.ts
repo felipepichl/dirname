53f91b91afcb5214cdbe5c6ec172facf5541eddf
Felipe Pichl <felipe.pichl@hotmail.com>
2023-07-28 22:41:32 -0300

    Triadge: Attendance Mappers

---
+++ projects/triadge/node-js/src/modules/attendance/infra/prisma/mappers/AttendanceMappers.ts
@@ -0,0 +1,26 @@
+import { Attendance } from '@modules/attendance/domain/Attendance';
+import { Attendance as RawAttendance } from '@prisma/client';
+
+import { IMapper } from '@shared/core/infra/Mapper';
+
+class AttendanceMappers implements IMapper<Attendance, RawAttendance> {
+  toPersistence(object: Attendance): Attendance {
+    return object;
+  }
+  toDomain({ date, present, fk_user_id }: RawAttendance): Attendance {
+    return Attendance.createAttendance({
+      date,
+      isPresent: present,
+      user_id: fk_user_id,
+    });
+  }
+  getMapper(): IMapper<Attendance, RawAttendance> {
+    return AttendanceMappers.getMapper();
+  }
+
+  static getMapper(): AttendanceMappers {
+    return new AttendanceMappers();
+  }
+}
+
+export { AttendanceMappers };
