5d332cc33c14bb25cca9de2c6dff33720e12f0d1
Felipe Pichl <felipe.pichl@hotmail.com>
2023-07-28 23:09:40 -0300

    Triadge: Doing Attendance Prisma

---
+++ projects/triadge/node-js/src/modules/attendance/infra/prisma/repositories/AttendanceRepository.ts
@@ -0,0 +1,47 @@
+import { User } from '@modules/accounts/domain/User';
+import { UserMappers } from '@modules/accounts/infra/prisma/mappers/UserMappers';
+import { Attendance } from '@modules/attendance/domain/Attendance';
+import { IAttendanceRepository } from '@modules/attendance/repositories/IAttendanceRepository';
+
+import { PrismaSingleton } from '@shared/infra/prisma';
+
+import { AttendanceMappers } from '../mappers/AttendanceMappers';
+
+class AttendanceRepository implements IAttendanceRepository {
+  async create({ date, isPresent, user_id }: Attendance): Promise<void> {
+    await PrismaSingleton.getInstance().attendance.create({
+      data: {
+        date,
+        present: isPresent,
+        fk_user_id: user_id,
+      },
+    });
+  }
+  async listAll(): Promise<Attendance[]> {
+    const result = await PrismaSingleton.getInstance().attendance.findMany();
+
+    return AttendanceMappers.getMapper().toDomain(result);
+  }
+  async listByUserId(user_id: string): Promise<User> {
+    const result = await PrismaSingleton.getInstance().attendance.findFirst({
+      where: { fk_user_id: user_id },
+      include: { users: true },
+    });
+
+    const { users } = result;
+
+    return UserMappers.getMapper().toDomain(users);
+  }
+  async listByDate(date: Date): Promise<User[]> {
+    const result = await PrismaSingleton.getInstance().attendance.findFirst({
+      where: { date },
+      include: { users: true },
+    });
+
+    const { users } = result;
+
+    return UserMappers.getMapper().toDomain(users);
+  }
+}
+
+export { AttendanceRepository };
