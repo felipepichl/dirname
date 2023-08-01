6064d5a9f743a6ea31a20f9ce7c07c6a7d0948a3
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-01 18:20:52 -0300

    Triadge: Attendance doing test e2e

---
+++ projects/triadge/node-js/src/modules/attendance/infra/http/controllers/createAttendance/CreateAttendanceController.e2e-spec.ts
@@ -0,0 +1,36 @@
+import request from 'supertest';
+
+import { app } from '@shared/infra/http/start/app';
+
+describe('[E2E] = Create Attendance', () => {
+  beforeEach(async () => {
+    await request(app).post('/users').send({
+      name: 'Jonh Due',
+      email: 'johndue@example.com',
+      password: 'hash123',
+      phoneNumber: '51999999999',
+    });
+  });
+
+  it('should be able to create a new attendance', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'johndue@example.com',
+      password: 'hash123',
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post('/attendances')
+      .set({
+        Authorization: `Bearer ${token}`,
+      })
+      .send({
+        data: new Date(),
+        isPresent: true,
+        user_id: 'user_not_found',
+      });
+
+    expect(response.status).toBe(201);
+  });
+});
--- projects/triadge/node-js/src/shared/infra/http/routes/index.ts
+++ projects/triadge/node-js/src/shared/infra/http/routes/index.ts
@@ -3,12 +3,15 @@ import { usersRouter } from '@modules/accounts/infra/http/routes/users.routes';
 import { attendancesRouter } from '@modules/attendance/infra/http/routes/attendances.routes';
 import { Router } from 'express';
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
 const router = Router();
 
 router.use('/users', usersRouter);
 
-router.use(authenticateRoutes);
+router.use('/sessions', authenticateRoutes);
 
-router.use(attendancesRouter);
+router.use(ensureAuthenticated);
+router.use('/attendances', attendancesRouter);
 
 export { router };
