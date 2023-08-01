20ae494ed47fa2cee02e5c4a02e43cc3cc34c540
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-01 17:27:55 -0300

    Triadge: CreateAttendanceController

---
+++ projects/triadge/node-js/src/modules/attendance/infra/http/controllers/createAttendance/CreateAttendanceController.ts
@@ -0,0 +1,21 @@
+import { CreateAttendanceUseCase } from '@modules/attendance/useCases/createAttendance/CreateAttendanceUseCase';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+class CreateAttendanceController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { date, isPresent, user_id } = request.body;
+
+    const createAttendanceUseCase = container.resolve(CreateAttendanceUseCase);
+
+    createAttendanceUseCase.execute({
+      date,
+      isPresent,
+      user_id,
+    });
+
+    return response.status(201).send();
+  }
+}
+
+export { CreateAttendanceController };
