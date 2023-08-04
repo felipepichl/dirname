fc5385de218f785a7bc86b4964df5c8a4adcd7a3
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-04 18:22:09 -0300

    Triadge: ListAllAttendancesController test Part II

--- projects/triadge/node-js/src/modules/attendance/useCases/listAllAttendances/ListAllAttendancesUseCase.ts
+++ projects/triadge/node-js/src/modules/attendance/useCases/listAllAttendances/ListAllAttendancesUseCase.ts
@@ -1,12 +1,17 @@
 import { Attendance } from '@modules/attendance/domain/Attendance';
 import { IAttendanceRepository } from '@modules/attendance/repositories/IAttendanceRepository';
+import { inject, injectable } from 'tsyringe';
 
 import { IUseCase } from '@shared/core/domain/IUseCase';
 
 type IResponse = Attendance[];
 
+@injectable()
 class ListAllAttendancesUseCase implements IUseCase<void, IResponse> {
-  constructor(private attendancesRepository: IAttendanceRepository) {}
+  constructor(
+    @inject('AttendanceRepository')
+    private attendancesRepository: IAttendanceRepository,
+  ) {}
 
   async execute(): Promise<IResponse> {
     const result = await this.attendancesRepository.listAll();
