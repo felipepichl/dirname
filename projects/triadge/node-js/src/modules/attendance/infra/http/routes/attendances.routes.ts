5bb7404bac750a308e00f5057ed03bb33e590291
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-04 18:07:47 -0300

    Triadge: ListAllAttendancesController routes

--- projects/triadge/node-js/src/modules/attendance/infra/http/routes/attendances.routes.ts
+++ projects/triadge/node-js/src/modules/attendance/infra/http/routes/attendances.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { CreateAttendanceController } from '../controllers/createAttendance/CreateAttendanceController';
+import { ListAllAttendancesController } from '../controllers/listAllAttendances/ListAllAttendancesController';
 
 const attendancesRouter = Router();
 
 const createAttendanceController = new CreateAttendanceController();
+const listAllAttendancesController = new ListAllAttendancesController();
 
 attendancesRouter.post('', createAttendanceController.handle);
+attendancesRouter.get('', listAllAttendancesController.handle);
 
 export { attendancesRouter };
