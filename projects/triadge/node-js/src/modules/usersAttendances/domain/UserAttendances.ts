f9842cd55c3b066b8ae59ac82ca383db75e19c6b
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-05 10:03:47 -0300

    Triadge: UserAttendance domain

---
+++ projects/triadge/node-js/src/modules/usersAttendances/domain/UserAttendances.ts
@@ -0,0 +1,43 @@
+import { User } from '@modules/accounts/domain/User';
+import { Attendance } from '@modules/attendance/domain/Attendance';
+
+import { AggregateRoot } from '@shared/core/domain/AggregateRoot';
+import { UniqueEntityID } from '@shared/core/domain/UniqueEntityID';
+
+interface IUserAttendanceProps {
+  user_id: string;
+  user?: User;
+  attendance_id: string;
+  attendance?: Attendance;
+}
+
+class UserAttendances extends AggregateRoot<IUserAttendanceProps> {
+  constructor(props: IUserAttendanceProps, id?: UniqueEntityID) {
+    super(props, id);
+  }
+
+  get user(): User {
+    return this.props.user;
+  }
+
+  get attendances(): Attendance {
+    return this.props.attendance;
+  }
+
+  static userAttendandeCreate({
+    user_id,
+    attendance_id,
+  }: IUserAttendanceProps): UserAttendances {
+    const userAttendancesProps = {
+      user_id,
+      attendance_id,
+    };
+
+    return AggregateRoot.create(
+      { props: userAttendancesProps },
+      UserAttendances,
+    );
+  }
+}
+
+export { UserAttendances };
