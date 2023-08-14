3d6f22e337813884454b9468d0ada4a43dfc968e
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-14 11:56:57 -0300

    Papa: Default Theme

--- projects/papa/mobile/App.tsx
+++ projects/papa/mobile/App.tsx
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 
 import { SignIn } from '@screens/SignIn';
+import { ThemeProvider } from 'styled-components/native';
+
+import theme from './src/theme'
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar 
         style='light'
         translucent
         backgroundColor='transparent'
         />
       <SignIn />
-    </>
+    </ThemeProvider>
   );
 }
---
+++ projects/papa/mobile/src/theme/index.ts
@@ -0,0 +1,33 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#F5F5F5',
+
+    // Dourado variando do mais escuro para o mais claro
+    PRIMARY_900: '#8D6A00',
+    PRIMARY_800: '#B28D1E',
+    PRIMARY_100: '#D4B15E',
+    PRIMARY_50:  '#EFD09C',
+
+    // Preto, mas não muito escuro, variando para tons de cinza
+    SECONDARY_900: '#282828',
+    SECONDARY_500: '#4A4A4A',
+    SECONDARY_400: '#6E6E6E',
+
+    SUCCESS_900: '#528F33',
+    SUCCESS_50: '#F7FFF9',
+
+    ALERT_900: '#B27F00',
+    ALERT_800: '#C5941A',
+    ALERT_50: '#F3EFE5',
+
+    SHAPE: '#B0B0B0',
+    TITLE: '#FFF',
+
+    GRADIENT: ['#8D6A00', '#B28D1E'],
+  },
+
+  FONTS: {
+    TITLE: 'DMSerifDisplay_400Regular',
+    TEXT: 'DMSans_400Regular',
+  },
+};
