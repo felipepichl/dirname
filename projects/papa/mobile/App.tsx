1ef5101bd0b3200bd40984aa8aefc97e81963616
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-14 11:30:54 -0300

    Papa: SignIn Part I

--- projects/papa/mobile/App.tsx
+++ projects/papa/mobile/App.tsx
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>PapaBurger</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar 
+        style='light'
+        translucent
+        backgroundColor='transparent'
+        />
+      <SignIn />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
