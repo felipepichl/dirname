d6eb91909750275b58e06f16c39cedb927e2192e
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-14 12:23:48 -0300

    Papa: SignIn Part II - css

--- projects/papa/mobile/src/screens/SignIn/index.tsx
+++ projects/papa/mobile/src/screens/SignIn/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Input } from '@components/Input'
+
+import { Container, Content, Title } from './styles';
 
 export function SignIn() {
   return (
-    <Container />
+    <Container>
+      <Content>
+
+      <Title>Login</Title>
+
+      <Input 
+        placeholder='E-mail'
+        type='secondary'
+        autoCorrect={false}
+        autoCapitalize='none'
+        // onChangeText={setEmail}
+      />
+
+      <Input 
+        placeholder='Senha'
+        type='secondary'
+        secureTextEntry
+        // onChangeText={setPassword}
+      />
+      </Content>
+    </Container>
   )
 }
