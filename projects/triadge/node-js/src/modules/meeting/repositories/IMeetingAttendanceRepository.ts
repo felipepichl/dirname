7f30f7d774405fa209fb56fb38ac6a17104b9553
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-15 14:57:05 -0300

    Triadge: Meeting IMeetingAttendanceRepository

---
+++ projects/triadge/node-js/src/modules/meeting/repositories/IMeetingAttendanceRepository.ts
@@ -0,0 +1,23 @@
+import { MeetingAttendance } from '../domain/MeetingAttendance';
+
+interface IMeetingAttendanceRepository {
+  create(meetingAttendance: MeetingAttendance): Promise<void>;
+  findAllByUserId(user_id: string): Promise<MeetingAttendance[]>;
+  findAllByAttendanceId(attendance_id: string): Promise<MeetingAttendance[]>;
+  findByUserIdAndAttendanceId(
+    user_id: string,
+    attendance_id: string,
+  ): Promise<MeetingAttendance | undefined>;
+  findByUserIdsAndAttendanceId(
+    user_ids: string[],
+    attendance_id: string,
+  ): Promise<MeetingAttendance[]>;
+  findByUserIdAndDate(
+    user_id: string,
+    date: Date,
+  ): Promise<MeetingAttendance | undefined>;
+  listByDate(date: Date): Promise<MeetingAttendance[]>;
+  listInDateRange(startDate: Date, endDate: Date): Promise<MeetingAttendance[]>;
+}
+
+export { IMeetingAttendanceRepository };
