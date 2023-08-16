3e7a9ecc44de1abec5490d8665d0051a51ae5e12
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-16 12:29:41 -0300

    Triadge: Meeting e2e test Part I

---
+++ projects/triadge/node-js/src/modules/meeting/infra/http/controller/createMeetingAttendance/CreateMeetingAttendanceController.e2e-spec.ts
@@ -0,0 +1,45 @@
+import { authConfig } from '@config/auth';
+import jwt from 'jsonwebtoken';
+import request from 'supertest';
+
+import { app } from '@shared/infra/http/start/app';
+
+async function authenticateUser() {
+  const response = await request(app).post('/sessions').send({
+    email: 'johndue@example.com',
+    password: 'hash123',
+  });
+  const { token } = response.body;
+  return token;
+}
+
+describe('[E2E] = Create Meeting', () => {
+  beforeAll(async () => {
+    await request(app).post('/users').send({
+      name: 'Jonh Due',
+      email: 'johndue@example.com',
+      password: 'hash123',
+      phoneNumber: '51999999999',
+    });
+  });
+
+  it('should be able to create a new metting', async () => {
+    const token = await authenticateUser();
+    const { sub: user_id } = jwt.verify(token, authConfig.secret_token);
+
+    const response = await request(app)
+      .post('/metting')
+      .set({
+        Authorization: `Bearer ${token}`,
+      })
+      .send({
+        date: new Date(),
+        isPresent: true,
+        user_id,
+      });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('message');
+    expect(response.body.message).toBe('Metting created');
+  });
+});
