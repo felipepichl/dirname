3331016790f0e8b3fbad9c919ae1cb9bb0230283
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-19 23:03:34 -0300

    Triadge: Accounts listAll controller

--- projects/triadge/node-js/src/modules/accounts/infra/http/routes/users.routes.ts
+++ projects/triadge/node-js/src/modules/accounts/infra/http/routes/users.routes.ts
@@ -1,11 +1,12 @@
-import { uploadConfig } from '@config/upload'
 import { Router } from 'express'
 import multer from 'multer'
 
+import { uploadConfig } from '@config/upload'
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated'
 
 import { CreateUserController } from '../controllers/createUser/CreateUserController'
 import { UploadUserAvatarController } from '../controllers/updateUserAvatar/UploadUserAvatarController'
+import { ListAllController } from '../controllers/listAll/ListAllController'
 
 const usersRouter = Router()
 
@@ -13,14 +14,18 @@ const uploadAvatar = multer(uploadConfig)
 
 const createUserController = new CreateUserController()
 const uploadUserAvatarController = new UploadUserAvatarController()
+const listAllController = new ListAllController()
 
 usersRouter.post('', createUserController.handle)
 
+usersRouter.use(ensureAuthenticated)
+
 usersRouter.patch(
   '/avatar',
-  ensureAuthenticated,
   uploadAvatar.single('avatar'),
   uploadUserAvatarController.handle,
 )
+
+usersRouter.get('', listAllController.handle)
 
 export { usersRouter }
