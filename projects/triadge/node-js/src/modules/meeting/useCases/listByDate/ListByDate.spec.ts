a69e05f6166312ae5749d1c6e634d785772d106b
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-21 15:19:36 -0300

    Triadge: Meeting, ListByDate test Part II

--- projects/triadge/node-js/src/modules/meeting/useCases/listByDate/ListByDate.spec.ts
+++ projects/triadge/node-js/src/modules/meeting/useCases/listByDate/ListByDate.spec.ts
@@ -5,9 +5,13 @@ import { AttendanceRepositoryInMemory } from '@modules/attendance/repositories/i
 import { MeetingAttendance } from '@modules/meeting/domain/MeetingAttendance'
 import { MeetingsAttendancesRepositoryInMemory } from '@modules/meeting/repositories/in-memory/MeetingsAttendancesRepositoryInMemory'
 
+import { ListByDate } from './ListByDate'
+
 let usersRepositoryInMemory: UsersRepositoryInMemory
 let attendanceRepositoryInMemory: AttendanceRepositoryInMemory
 let meetingsAttendancesRepositoryInMemory: MeetingsAttendancesRepositoryInMemory
+
+let listByDate: ListByDate
 
 describe('[Meeting] - List Meeting Attendance by Date', () => {
   beforeEach(() => {
@@ -15,6 +19,8 @@ describe('[Meeting] - List Meeting Attendance by Date', () => {
     attendanceRepositoryInMemory = new AttendanceRepositoryInMemory()
     meetingsAttendancesRepositoryInMemory =
       new MeetingsAttendancesRepositoryInMemory()
+
+    listByDate = new ListByDate(meetingsAttendancesRepositoryInMemory)
   })
 
   it('should be able to list all meeting by date', async () => {
@@ -32,8 +38,16 @@ describe('[Meeting] - List Meeting Attendance by Date', () => {
       phoneNumber: '123456789',
     })
 
+    const user3 = User.createUser({
+      name: 'Test User3',
+      email: 'user3@test.com',
+      password: '123456',
+      phoneNumber: '12345678910',
+    })
+
     await usersRepositoryInMemory.create(user1)
     await usersRepositoryInMemory.create(user2)
+    await usersRepositoryInMemory.create(user3)
 
     const attendanceDate = new Date(2022, 3, 16)
     const attendance = Attendance.createAttendance({
@@ -52,11 +66,23 @@ describe('[Meeting] - List Meeting Attendance by Date', () => {
 
     const userIds = [userId1.toString(), userId2.toString()]
 
-    const mettingsAttendances = MeetingAttendance.createMeetingAttendance({
+    const meetingsAttendances = MeetingAttendance.createMeetingAttendance({
       userIds,
       attendanceId: attendanceId.toString(),
     })
 
-    meetingsAttendancesRepositoryInMemory.create(mettingsAttendances)
+    await meetingsAttendancesRepositoryInMemory.create(meetingsAttendances)
+
+    const meetingsForGivenDate = await listByDate.execute({
+      date: attendanceDate,
+    })
+
+    expect(meetingsForGivenDate.mettingsAttendances).toHaveLength(1)
+    expect(meetingsForGivenDate.mettingsAttendances[0].userIds).toEqual(
+      expect.arrayContaining([userId1, userId2]),
+    )
+    expect(meetingsForGivenDate.mettingsAttendances[0].userIds).not.toContain(
+      user3.id,
+    )
   })
 })
