04b3f2dd2d25e69d0fd26364781693e7e3a49c78
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-21 18:03:17 -0300

    Triadge: Meeting, ListMeetingsByDate controller

---
+++ projects/triadge/node-js/src/modules/meeting/infra/http/controller/listMeetingsByDate/ListMeetingsByDateController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+import { ListMeetingsByDate } from '@modules/meeting/useCases/listMeetingsByDate/ListMeetingsByDate'
+
+class ListMeetingsByDateController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { date } = request.body
+
+    const listMeetingsByDate = container.resolve(ListMeetingsByDate)
+
+    const result = listMeetingsByDate.execute(date)
+
+    return response.status(200).json(result)
+  }
+}
+
+export { ListMeetingsByDateController }
