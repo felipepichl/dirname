47186b0919710f09f9e5175d00087142f75ac0b6
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-22 18:11:04 -0300

    Triadge: ListMeetingsByDate test:e2e Part II

--- projects/triadge/node-js/src/modules/meeting/infra/http/routes/meetings.routes.ts
+++ projects/triadge/node-js/src/modules/meeting/infra/http/routes/meetings.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express'
 
 import { CreateMeetingAttendanceController } from '../controller/createMeetingAttendance/CreateMeetingAttendanceController'
+import { ListMeetingsByDateController } from '../controller/listMeetingsByDate/ListMeetingsByDateController'
 
 const meetingRoutes = Router()
 
 const createMeetingAttendanceController =
   new CreateMeetingAttendanceController()
 
+const listMeetingsByDateController = new ListMeetingsByDateController()
+
 meetingRoutes.post('', createMeetingAttendanceController.handle)
+meetingRoutes.get('', listMeetingsByDateController.handle)
 
 export { meetingRoutes }
