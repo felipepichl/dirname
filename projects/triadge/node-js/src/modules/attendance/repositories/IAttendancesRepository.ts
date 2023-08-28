6a78e1ed5715a04587354733d1e022347c319564
Felipe Pichl <felipe.pichl@hotmail.com>
2023-08-28 18:20:58 -0300

    Triadge: Fix IAttendancesRepository

---
+++ projects/triadge/node-js/src/modules/attendance/repositories/IAttendancesRepository.ts
@@ -0,0 +1,12 @@
+import { Attendance } from '../domain/Attendance'
+
+interface IAttendancesRepository {
+  create(attendance: Attendance): Promise<void>
+  findById(id: string): Promise<Attendance>
+  findAll(): Promise<Attendance[]>
+  findByUserId(userId: string): Promise<Attendance[]>
+  findByMeetingId(meetingId: string): Promise<Attendance[]>
+  findAttendance(userId: string, meetingId: string): Promise<Attendance>
+}
+
+export { IAttendancesRepository }
