2c3ac8cd13e81e648578aa0d62b3512d10f47951
Felipe Pichl <felipe.pichl@hotmail.com>
2023-09-01 14:44:52 -0300

    Triadge: ListAllMeetingController - Part I

---
+++ projects/triadge/node-js/src/modules/meeting/infra/http/controllers/listAllMeeting/ListAllMeetingController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+import { ListAllMeeting } from '@modules/meeting/useCases/listAllMeeting/ListAllMeeting'
+
+class ListAllMeetingController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const listAllMeeting = container.resolve(ListAllMeeting)
+
+    const meetings = await listAllMeeting.execute()
+
+    return response.json({ meetings })
+  }
+}
+
+export { ListAllMeetingController }
--- projects/triadge/node-js/src/modules/meeting/infra/http/routes/meetings.routes.ts
+++ projects/triadge/node-js/src/modules/meeting/infra/http/routes/meetings.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 
 import { CreateMeetingController } from '../controllers/createMeeting/CreateMeetingController'
+import { ListAllMeetingController } from '../controllers/listAllMeeting/ListAllMeetingController'
 
 const meetingsRouter = Router()
 
 const createMeetingController = new CreateMeetingController()
+const listAllMeetingController = new ListAllMeetingController()
 
 meetingsRouter.post('', createMeetingController.handle)
+meetingsRouter.get('', listAllMeetingController.handle)
 
 export { meetingsRouter }
