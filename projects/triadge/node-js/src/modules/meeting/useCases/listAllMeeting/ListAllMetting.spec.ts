ebe54a576a5be9fe0f2b28582b7005adce006375
Felipe Pichl <felipe.pichl@hotmail.com>
2023-09-01 11:02:34 -0300

    Triadge: ListAllMeeting - test Part I

---
+++ projects/triadge/node-js/src/modules/meeting/useCases/listAllMeeting/ListAllMetting.spec.ts
@@ -0,0 +1,28 @@
+import { Meeting } from '@modules/meeting/domain/Meeting'
+import { MeetingRepositoryInMemory } from '@modules/meeting/repositories/in-memory/MeetingRepositoryInMemory'
+
+import { ListAllMeeting } from './ListAllMeeting'
+
+let meetingsRepositoryInMemory: MeetingRepositoryInMemory
+let listAllMeeting: ListAllMeeting
+
+describe('[Meeting] - List all meetings', () => {
+  beforeEach(() => {
+    meetingsRepositoryInMemory = new MeetingRepositoryInMemory()
+
+    listAllMeeting = new ListAllMeeting(meetingsRepositoryInMemory)
+  })
+
+  it('should be able to list all meetings', async () => {
+    const meeting1 = Meeting.createMeeting({
+      date: new Date(),
+    })
+
+    const meeting2 = Meeting.createMeeting({
+      date: new Date(),
+    })
+
+    await meetingsRepositoryInMemory.create(meeting1)
+    await meetingsRepositoryInMemory.create(meeting2)
+  })
+})
