de5e9230125dca350d32682a24f139190a47ac51
Felipe Pichl <felipe.pichl@hotmail.com>
2023-09-01 09:21:30 -0300

    Triadge: Clean project

--- projects/triadge/node-js/src/modules/attendance/infra/http/routes/attendances.routes.ts
+++ projects/triadge/node-js/src/modules/attendance/infra/http/routes/attendances.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 
-import { CreateAttendanceController } from '../controller/createMeetingAttendance/CreateAttendanceController'
+import { CreateAttendanceController } from '../controller/createAttendance/CreateAttendanceController'
 
 const attendancesRouter = Router()
 
