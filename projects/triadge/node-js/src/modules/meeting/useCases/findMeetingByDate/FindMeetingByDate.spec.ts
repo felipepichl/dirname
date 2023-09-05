fa9004698f8645f9d75915d3fb34c5232574416e
Felipe Pichl <felipe.pichl@hotmail.com>
2023-09-05 12:20:19 -0300

    Triadge: Refactoring CreateMeeting test Part III

--- projects/triadge/node-js/src/modules/meeting/useCases/findMeetingByDate/FindMeetingByDate.spec.ts
+++ projects/triadge/node-js/src/modules/meeting/useCases/findMeetingByDate/FindMeetingByDate.spec.ts
@@ -1,16 +1,18 @@
+import { User } from '@modules/accounts/domain/User'
 import { Meeting } from '@modules/meeting/domain/Meeting'
 import { Attendance } from '@modules/attendance/domain/Attendance'
 
 import { UsersRepositoryInMemory } from '@modules/accounts/repositories/in-memory/UsersRepositoryInMemory'
 import { MeetingRepositoryInMemory } from '@modules/meeting/repositories/in-memory/MeetingRepositoryInMemory'
+import { AttendancesRepositoryInMemory } from '@modules/attendance/repositories/in-memory/AttendancesRepositoryInMemory'
 
 import { FindMeetingByDate } from './FindMeetingByDate'
 
 import { AppError } from '@shared/error/AppError'
-import { User } from '@modules/accounts/domain/User'
 
 let meetingsRepositoryInMemory: MeetingRepositoryInMemory
 let findMeetingByDate: FindMeetingByDate
+let meetingDate: Date
 
 async function createUser(
   name: string,
@@ -46,10 +48,52 @@ async function createMeeting(date: Date): Promise<Meeting> {
   return result[0]
 }
 
+async function createAttendance(
+  users: User[],
+  meeting: Meeting,
+): Promise<Attendance> {
+  const userIds = [users[0].id.toString(), users[1].id.toString()]
+  const meetingId = meeting.id.toString()
+
+  const attendance = Attendance.createAttendance({
+    userIds,
+    meetingId,
+    user: [users[0], users[1]],
+    meeting,
+  })
+
+  const attendancesRepository = new AttendancesRepositoryInMemory()
+
+  await attendancesRepository.create(attendance)
+
+  const result = await attendancesRepository.findAll()
+
+  return result[0]
+}
+
 describe('[Meeting] - Find meeting by date', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     meetingsRepositoryInMemory = new MeetingRepositoryInMemory()
     findMeetingByDate = new FindMeetingByDate(meetingsRepositoryInMemory)
+
+    const user1 = await createUser(
+      'User1',
+      'user1@example.com',
+      'password123',
+      '1234567890',
+    )
+
+    const user2 = await createUser(
+      'User2',
+      'user2@example.com',
+      'password123',
+      '1234567890',
+    )
+
+    meetingDate = new Date(2022, 3, 16)
+    const meeting = await createMeeting(meetingDate)
+
+    await createAttendance([user1, user2], meeting)
   })
 
   it('should return a meeting and its attendees when found by date', async () => {
@@ -79,26 +123,6 @@ describe('[Meeting] - Find meeting by date', () => {
     // const attendeeIds = retrievedAttendances.map((attendee) => attendee.id)
     // expect(attendeeIds).toContain('fakeUser1')
     // expect(attendeeIds).toContain('fakeUser2')
-
-    const user1 = await createUser(
-      'User1',
-      'user1@example.com',
-      'password123',
-      '1234567890',
-    )
-
-    const user2 = await createUser(
-      'User2',
-      'user2@example.com',
-      'password123',
-      '1234567890',
-    )
-
-    const meetingDate = new Date(2022, 3, 16)
-    const meeting = await createMeeting(meetingDate)
-
-    console.log(user1, user2)
-    console.log(meeting)
   })
 
   it('should throw an AppError if no meeting is found for the specified date', async () => {
