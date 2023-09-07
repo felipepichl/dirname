b3e9bc5208ad15d867db38dffa3b7d6ed76b7c30
Felipe Pichl <felipe.pichl@hotmail.com>
2023-09-07 20:50:36 -0300

    Triadge: Update user routes

--- projects/triadge/node-js/src/modules/accounts/infra/http/routes/users.routes.ts
+++ projects/triadge/node-js/src/modules/accounts/infra/http/routes/users.routes.ts
@@ -5,6 +5,7 @@ import { uploadConfig } from '@config/upload'
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated'
 
 import { CreateUserController } from '../controllers/createUser/CreateUserController'
+import { CreateUserPartialController } from '../controllers/createUserPartial/CreateUserPartialController'
 import { UploadUserAvatarController } from '../controllers/updateUserAvatar/UploadUserAvatarController'
 import { ListAllController } from '../controllers/listAll/ListAllController'
 
@@ -13,6 +14,7 @@ const usersRouter = Router()
 const uploadAvatar = multer(uploadConfig)
 
 const createUserController = new CreateUserController()
+const createUserPartialController = new CreateUserPartialController()
 const uploadUserAvatarController = new UploadUserAvatarController()
 const listAllController = new ListAllController()
 
@@ -20,12 +22,12 @@ usersRouter.post('', createUserController.handle)
 
 usersRouter.use(ensureAuthenticated)
 
+usersRouter.post('/partial', createUserPartialController.handle)
 usersRouter.patch(
   '/avatar',
   uploadAvatar.single('avatar'),
   uploadUserAvatarController.handle,
 )
-
 usersRouter.get('', listAllController.handle)
 
 export { usersRouter }
