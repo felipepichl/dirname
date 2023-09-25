c0d78b9687a06442ab3217e617a13237d12d6893
Felipe Pichl <felipe.pichl@hotmail.com>
2023-09-25 15:35:06 -0300

    Triadge: Lodge, Done controller for creation

--- projects/triadge/node-js/src/modules/lodge/infra/http/controller/createLodge/CreateLoadgeController.ts
+++ projects/triadge/node-js/src/modules/lodge/infra/http/controller/createLodge/CreateLoadgeController.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+import { CreateLodgeUseCase } from '@modules/lodge/useCases/createLodge/CreateLodgeUseCase'
 
 class CreateLoadgeController {
-  async handle(request: Request, response: Response): Promise<Response> {}
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, foundingDate, isActive } = request.body
+
+    const createLodgeUseCase = container.resolve(CreateLodgeUseCase)
+
+    await createLodgeUseCase.execute({
+      name,
+      foundingDate,
+      isActive,
+    })
+
+    return response.status(201).json({ message: 'Lodge created' })
+  }
 }
 
 export { CreateLoadgeController }
