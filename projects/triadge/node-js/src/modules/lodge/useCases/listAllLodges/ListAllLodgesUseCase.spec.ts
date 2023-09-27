ebf5b9911bf84c8c7a658d0137c61311d59f0b16
Felipe Pichl <felipe.pichl@hotmail.com>
2023-09-27 15:13:41 -0300

    Triadge: Lodge, useCase done test

--- projects/triadge/node-js/src/modules/lodge/useCases/listAllLodges/ListAllLodgesUseCase.spec.ts
+++ projects/triadge/node-js/src/modules/lodge/useCases/listAllLodges/ListAllLodgesUseCase.spec.ts
@@ -14,13 +14,13 @@ describe('[Lodge] - List all lodges', () => {
 
   it('should be able to list all lodges', async () => {
     const lodge1 = Lodge.createLodge({
-      name: 'lodge_name',
+      name: 'lodge_name_1',
       foundingDate: new Date(),
       isActive: true,
     })
 
     const lodge2 = Lodge.createLodge({
-      name: 'lodge_name',
+      name: 'lodge_name_2',
       foundingDate: new Date(),
       isActive: true,
     })
@@ -28,6 +28,26 @@ describe('[Lodge] - List all lodges', () => {
     await lodgesRepositoryInMemory.create(lodge1)
     await lodgesRepositoryInMemory.create(lodge2)
 
+    const { lodges } = await listAllLodgesUseCase.execute()
+
+    expect(lodges).toHaveLength(2)
+    expect(lodges).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: lodge1.id,
+          name: 'lodge_name_1',
+        }),
+        expect.objectContaining({
+          id: lodge2.id,
+          name: 'lodge_name_2',
+        }),
+      ]),
+    )
+  })
+
+  it('should return an empty array if no lodges exist', async () => {
     const result = await listAllLodgesUseCase.execute()
+
+    expect(result.lodges).toHaveLength(0)
   })
 })
