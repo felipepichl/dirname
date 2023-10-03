1e1d81c8010e51aae24e0a838f8c3e957d448def
Felipe Pichl <felipe.pichl@hotmail.com>
2023-10-03 17:32:01 -0300

    Triadge: Lodge, AddMemberToLodge usecase

---
+++ projects/triadge/node-js/src/modules/lodge/useCases/addMemberToLodge/AddMemberToLodgeUseCase.ts
@@ -0,0 +1,33 @@
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository'
+import { ILodgesRepository } from '@modules/lodge/repositories/ILodgesRepository'
+
+import { IUseCase } from '@shared/core/domain/IUseCase'
+import { AppError } from '@shared/error/AppError'
+
+interface IRequest {
+  lodgeId: string
+  userId: string
+}
+
+class AddMemberToLodgeUseCase implements IUseCase<IRequest, void> {
+  constructor(
+    private lodgesRepository: ILodgesRepository,
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ lodgeId, userId }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(userId)
+
+    if (!user) {
+      throw new AppError('User not found', 404)
+    }
+
+    const lodge = await this.lodgesRepository.findById(lodgeId)
+
+    if (!lodge) {
+      throw new AppError('Lodge not found', 404)
+    }
+  }
+}
+
+export { AddMemberToLodgeUseCase }
