2a851cd5654573f97419332b18341713bdda96f5
Felipe Pichl <felipe.pichl@hotmail.com>
2023-10-06 18:05:36 -0300

    Triadge: AddMemberToLodge test-e2e Part I

---
+++ projects/triadge/node-js/src/modules/lodge/infra/http/controller/addMemberToLodge/AddMemberToLodgeController.e2e-spec.ts
@@ -0,0 +1,84 @@
+import request from 'supertest'
+
+import { app } from '@shared/infra/http/start/app'
+
+async function authenticateUser() {
+  await request(app).post('/users').send({
+    name: 'Jonh Due',
+    email: 'johndue@example.com',
+    password: 'hash123',
+    phoneNumber: '51999999999',
+  })
+
+  const response = await request(app).post('/sessions').send({
+    email: 'johndue@example.com',
+    password: 'hash123',
+  })
+  const { token } = response.body
+  return token
+}
+
+async function createUserAndListAll(token: string): Promise<string> {
+  await request(app).post('/users').send({
+    name: 'User First',
+    email: 'user1@example.com',
+    password: 'hash123',
+    phoneNumber: '51999999999',
+  })
+
+  const response = await request(app)
+    .get('/users')
+    .set({
+      Authorization: `Bearer ${token}`,
+    })
+
+  type UserResponse = {
+    _id: string
+  }
+
+  const user: UserResponse[] = response.body.users
+
+  return user[0]._id
+}
+
+async function createLodgeAndListAll(token: string): Promise<string> {
+  await request(app)
+    .post('/lodges')
+    .set({
+      Authorization: `Bearer ${token}`,
+    })
+    .send({
+      name: 'Lodge_Name',
+      foundingDate: new Date(2022, 3, 16),
+      isActive: true,
+    })
+
+  const response = await request(app)
+    .get('/lodges')
+    .set({
+      Authorization: `Bearer ${token}`,
+    })
+
+  type LodgeResponse = {
+    _id: string
+  }
+
+  const lodge: LodgeResponse[] = response.body.lodges
+
+  return lodge[0]._id
+}
+
+describe('[E2E] = Add member to Lodge', () => {
+  it('should be able to add member to Lodge', async () => {
+    const token = await authenticateUser()
+    const member = await createUserAndListAll(token)
+    const lodge = await createLodgeAndListAll(token)
+
+    // console.log(member)
+    // console.log(lodge)
+
+    // expect(response.status).toBe(201)
+    // expect(response.body).toHaveProperty('message')
+    // expect(response.body.message).toBe('Lodge created')
+  })
+})
