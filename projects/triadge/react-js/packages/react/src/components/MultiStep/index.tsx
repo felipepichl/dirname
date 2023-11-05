0620d28038767afad974d83790ca2b6d839ab73e
Felipe Pichl <felipe.pichl@hotmail.com>
2023-11-05 19:51:11 -0300

    Triadge: Component:MultiStep

---
+++ projects/triadge/react-js/packages/react/src/components/MultiStep/index.tsx
@@ -0,0 +1,20 @@
+import { Label, MultiSpetContainer, Steps, Step } from "./styles";
+
+export interface MultiStepProps {
+  size: number
+  currentStep?: number
+}
+
+export function MultiStep({ size, currentStep = 1 }: MultiStepProps) {
+  return (
+    <MultiSpetContainer>
+      <Label>Passo {currentStep} de {size}</Label>
+
+      <Steps css={{ '--steps-size': size }}>
+        {Array.from({ length: size }, (_, i) => i + 1).map(step => {
+          return <Step key={step} active={currentStep >= step}/>
+        })}
+      </Steps>
+    </MultiSpetContainer>
+  )
+}
