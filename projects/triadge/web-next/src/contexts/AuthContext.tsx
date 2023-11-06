920520df934a1a2a9bae30d3278faf41858e8752
Felipe Pichl <felipe.pichl@hotmail.com>
2023-11-05 22:35:08 -0300

    Triadge-Next: Context

---
+++ projects/triadge/web-next/src/contexts/AuthContext.tsx
@@ -0,0 +1,31 @@
+import { ReactNode, createContext } from 'react'
+
+type SignInCreadentials = {
+  email: string
+  password: string
+}
+
+type AuthContextData = {
+  signIn(credentials: SignInCreadentials): Promise<void>
+  isAuthenticated: boolean
+}
+
+type AuthProviderProps = {
+  children: ReactNode
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const isAuthenticated = false
+
+  async function signIn({email, password}: SignInCreadentials): Promise<void> {
+    console.log(email, password)
+  }
+  
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
