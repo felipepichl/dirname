faa8a3272aebd2dcedcdcc112d465082a47a5ec7
Felipe Pichl <felipe.pichl@hotmail.com>
2023-11-06 23:14:31 -0300

    Triadge-Next: GlobalStyles

--- projects/triadge/web-next/src/pages/_app.tsx
+++ projects/triadge/web-next/src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import { AuthProvider } from '@/contexts/AuthContext'
+import { globalStyles } from '@/styles/global'
 
 import type { AppProps } from 'next/app'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- projects/triadge/web-next/src/styles/index.ts
+++ projects/triadge/web-next/src/styles/index.ts
@@ -12,7 +12,15 @@ export const {
 } = createStitches({
   theme: {
     colors: {
-      triadge: '#7159C1'
+      white: '#FFF',
+
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#C4C4CC',
+      gray100: '#E1E1E6',
+      
+      green500: '#00875f',
+      green300: '#00b37e',
     }
   }
 })
