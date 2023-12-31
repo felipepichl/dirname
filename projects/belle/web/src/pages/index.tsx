7d703365276b781cfdaef8525e7aceb22cb6c3e2
Felipe Pichl <felipe.pichl@hotmail.com>
2023-12-31 08:48:05 -0300

    feat: Implement home css

--- projects/belle/web/src/pages/index.tsx
+++ projects/belle/web/src/pages/index.tsx
@@ -1,9 +1,22 @@
-import { Inter } from 'next/font/google'
+import Image from "next/image"
 
-const inter = Inter({ subsets: ['latin'] })
+import { HomeContainer, Product } from "@/styles/pages/home"
+
+import shirt1 from '@/assets/camisetas/1.png'
+import shirt2 from '@/assets/camisetas/2.png'
+import shirt3 from '@/assets/camisetas/3.png'
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt=""/>
+        
+        <footer>
+          <strong>Shirt 1</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
