12c26f004185ae72f55940760a1656da2ae0109c
Felipe Pichl <felipe.pichl@hotmail.com>
2024-01-18 16:26:22 -0300

    fix: Swiper types

---
+++ projects/belle/web/src/@types/swiper/index.d.ts
@@ -0,0 +1,7 @@
+declare module 'swiper' {
+  import SwiperCore, { Navigation, Pagination, EffectCoverflow } from 'swiper';
+  import 'swiper/swiper.scss';
+
+  export default SwiperCore;
+  export { Navigation, Pagination, EffectCoverflow };
+}
